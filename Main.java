9b237fcf21c316a219fe148d77f16b7a63f17216
Sedat Ay <159232350+SedatAy1@users.noreply.github.com>
2024-03-31 22:20:37 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            FileReader fileReader = new FileReader("notes.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String textRead;
+            while ((textRead = bufferedReader.readLine()) != null){
+                System.out.println(textRead);
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter text: ");
+        String textWrite = s.nextLine();
+        try {
+            FileWriter file = new FileWriter("notes.txt",true);
+            PrintWriter printWriter = new PrintWriter(file);
+            printWriter.println();
+            printWriter.print(textWrite);
+            printWriter.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
